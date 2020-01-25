682131561ead3d54cafe00bff2d1d0cdf7c8c0fa
David Krause <david@code0.xyz>
2020-01-25 20:58:46 +0100

    Signed-off-by: David Krause <david@code0.xyz>

---
+++ classicColor.nim
@@ -0,0 +1,23 @@
+import math, strformat
+
+type Color* = tuple[r: int, g: int, b: int]
+
+proc computeColor*(str: string): Color =
+  ## computes rgb from string
+  var commulated = 0;
+  for ch in str:
+    commulated += ch.ord
+  var communist = commulated mod 235 + 20
+  return (
+    int communist, 
+    int communist.float32.pow(2.0) mod 255, 
+    int communist.float32.pow(3.0) mod 255
+  )
+
+proc forCss*(color: Color): string =
+  ## converts color to "rgb(r,g,b)" string
+  fmt"rgb({color.r},{color.g},{color.b})"
+
+when isMainModule:
+  assert computeColor("sn0re") == (38,169,47)
+  assert computeColor("sn0re").forCss == "rgb(38,169,47)"
---
+++ nimClassicColor.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "David Krause"
+description   = "classical code0 colors"
+license       = "MIT"
+srcDir        = "src"
+
+
+
+# Dependencies
+
+requires "nim >= 1.0.4"
